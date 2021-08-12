d8ef569ba66ec03b215d4fa38e6742f6a51ce5a0
Lincoln Yan <yyjlincoln@gmail.com>
2021-08-12 20:31:26 +1000

    Update: Added placeholder page

---
+++ src/components/placeholder.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="flex relative w-full h-full min-w-10 min-h-10 flex-col justify-center">
+      <box-icon name="traffic-cone" class="mx-auto w-16 h-16" color="#157f1f" type="solid"> </box-icon>
+      <text-styles class="mx-auto text-center mt-3" type="subtitle_color">Under construction</text-styles>
+    </div>
+  </div>
+</template>
+
+<script>
+import textStyles from "./text-styles.vue";
+export default {
+  components: { textStyles },
+};
+</script>
+
+<style>
+</style>
--- src/views/cv/curriculum-vitae.vue
+++ src/views/cv/curriculum-vitae.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-      <page-frame>
-          
-      </page-frame>
+    <page-frame>
+      <placeholder class="h-screen"></placeholder>
+      
+    </page-frame>
   </div>
 </template>
 
 <script>
-import pageFrame from '../../components/page-frame.vue'
+import pageFrame from "../../components/page-frame.vue";
+import Placeholder from "../../components/placeholder.vue";
 export default {
-  components: { pageFrame },
-
-}
+  components: { pageFrame, Placeholder },
+};
 </script>
 
 <style>
-
 </style>
