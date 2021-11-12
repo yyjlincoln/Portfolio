b2caa5fae38887b360aea4a96e14987ac9784df0
Lincoln Yan <yyjlincoln@gmail.com>
2021-11-12 18:55:52 +1100

    Update: Added UNSW Scholarship page

--- src/router/index.js
+++ src/router/index.js
@@ -175,6 +175,18 @@ const MAIN_ROOTS = [
                 }
             },
             {
+                path: 'unsw',
+                name: 'unsw',
+                component: Scholarships,
+                meta: {
+                    nav: {
+                        available: false,
+                        name: "Additional Information & Supporting Evidence for the Scholarship Application"
+                    },
+                    title: "YIJUN YAN - Additional Information & Supporting Evidence for the Scholarship Application"
+                }
+            },
+            {
                 path: 'eald',
                 name: 'eald',
                 component: GoEALD,
