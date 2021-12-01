3d96df5753ccfb1a18f30073377726eea06d8f92
Lincoln Yan <yyjlincoln@gmail.com>
2021-12-01 22:30:17 +1100

    Update: Temporarily hide Utilities/Plugins

--- src/components/work-demo.vue
+++ src/components/work-demo.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="projects">
-    <!-- Projects -->
-    <text-styles
-      type="medium primary_color extrabold"
-      class="text-center mt-10"
-    >
-      Projects
-    </text-styles>
-    <div class="my-5">
-      <!-- Project Cards -->
-      <div class="flex flex-col justify-center md:flex-row flex-wrap w-full">
-        <div
-          v-for="project in $commondata.projects"
-          :key="project.name"
-          class="projectcard"
-        >
-          <project-card
-            :project="project"
-            class=""
-            :brief="brief"
-          ></project-card>
+    <div>
+      <!-- Projects -->
+      <text-styles
+        type="medium primary_color extrabold"
+        class="text-center mt-10"
+      >
+        Projects
+      </text-styles>
+      <div class="my-5">
+        <!-- Project Cards -->
+        <div class="flex flex-col justify-center md:flex-row flex-wrap w-full">
+          <div
+            v-for="project in $commondata.projects"
+            :key="project.name"
+            class="projectcard"
+          >
+            <project-card
+              :project="project"
+              class=""
+              :brief="brief"
+            ></project-card>
+          </div>
         </div>
       </div>
     </div>
-    <text-styles
-      type="medium primary_color extrabold"
-      class="text-center mt-10"
-    >
-      Utilities / Plugins
-    </text-styles>
-    <div class="my-5">
-      <div class="flex flex-col justify-center md:flex-row flex-wrap w-full">
-        <div
-          v-for="project in $commondata.utilities"
-          :key="project.name"
-          class="projectcard"
-        >
-          <project-card
-            :project="project"
-            class=""
-            :brief="brief"
-          ></project-card>
+    <div v-if="false"> 
+      <!-- Currently under development. Not available yet. -->
+      <!-- Utilities -->
+      <text-styles
+        type="medium primary_color extrabold"
+        class="text-center mt-10"
+      >
+        Utilities / Plugins
+      </text-styles>
+      <div class="my-5">
+        <div class="flex flex-col justify-center md:flex-row flex-wrap w-full">
+          <div
+            v-for="project in $commondata.utilities"
+            :key="project.name"
+            class="projectcard"
+          >
+            <project-card
+              :project="project"
+              class=""
+              :brief="brief"
+            ></project-card>
+          </div>
         </div>
       </div>
     </div>
